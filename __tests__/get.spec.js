ffa964d406406a53f730a7848f4354b241db1280
Cierra Morris <4cibe@MacBook-Pro-10.localdomain>
2022-03-10 13:46:42 -0700

    added solution test start

--- __tests__/get.spec.js
+++ __tests__/get.spec.js
@@ -1,5 +1,19 @@
-describe('GET requests', () => {
-    it('get all users', () => {
-        expect(true).toEqual(true);
-    });
-});
+const app = require('../app')
+const supertest = require('supertest');
+const { expect } = require('@jest/globals');
+const request = supertest(app)
+
+
+describe('Test Handlers', () => {
+    test('responds to /', async () => {
+        const res = await request.get('/');
+        expect(res.header['content-type']).toBe('application/json; charset=utf-8');
+        expect(res.statusCode).toBe(200)
+    })
+
+    test('responds to /users', async () => {
+        const res = await request.get('/users');
+        expect(res.header['content-type']).toBe('application/json; charset=utf-8');
+        expect(res.statusCode).toBe(200)
+    })
+})
--- __tests__/post.spec.js
+++ __tests__/post.spec.js
@@ -1,5 +1,15 @@
-describe('POST requests', () => {
-    it('post a user', () => {
-        expect(true).toEqual(true);
-    });
-});
+const app = require('../app')
+const supertest = require('supertest');
+const { expect } = require('@jest/globals');
+const request = supertest(app)
+
+
+describe('Test Handlers', () => {
+    test('responds to post /users', async () => {
+        const res = await request.post('/users');
+        expect(res.header['content-type']).toBe('application/json; charset=utf-8');
+        expect(res.statusCode).toBe(201)
+    })
+
+    
+})
